47d8e2fb726cf3ed6b36ebf16b6a2f40aeba7311
Sean McGary <sean.mcgary@gmail.com>
2012-05-16 19:59:05 -0700

    starting server. client with error handling for malformed json working

---
+++ config/nodequeue.js
@@ -0,0 +1,22 @@
+exports.commands = {
+	use: 0,
+	push: 1,
+	watch: 2,
+	ignore: 3,
+	reserve: 4,
+	pop: 5,
+	delete: 6,
+	peek: 7,
+	stat: 8,
+	server_stat: 9,
+	delete_channel: 10,
+	clear_channel: 11
+};
+
+exports.codes = {
+	// 1xx - error codes
+	"100": "ERR 100 Malformed JSON request" 
+
+	// 2xx response
+
+};
