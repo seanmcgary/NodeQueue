46cc11f333b38305bfcf9540ffc883d8cb926ff0
Sean McGary <sean.mcgary@gmail.com>
2012-05-17 14:03:02 -0700

    finishing up implementing protocol

--- config/nodequeue.js
+++ config/nodequeue.js
@@ -11,7 +11,9 @@ exports.commands = {
 	server_stat: 9,
 	delete_channel: 10,
 	clear_channel: 11,
-	stop_use: 12
+	stop_use: 12,
+	broadcase: 13,
+	channel_queue: 14
 };
 
 exports.codes = {
@@ -19,13 +21,19 @@ exports.codes = {
 	"100": "ERR 100 Malformed JSON request",
 	"101": "ERR 101 Channel doesn't exist",
 	"102": "ERR Queue is empty",
+	"103": "ERR Message for msg_id not in queue"
 
 	// 2xx - success response
 	"200": "OK Subscribed to channel",
 	"201": "OK Pushed message to channel",
 	"202": "OK Channel updated",
 	"203": "OK Message reserved",
-	"204": "OK Watching channel"
+	"204": "OK Watching channel",
+	"205": "OK Message popped",
+	"206": "OK Message peeked",
+	"207": "OK Cleared channel",
+	"208": "OK Channel deleted",
+	"209": "OK Server stat"
 };
 
 exports.msg_states = {
