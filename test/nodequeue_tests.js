e9c1c4b477f24fb8aaed1def268d3ad689e4c2a6
Sean McGary <sean.mcgary@gmail.com>
2012-05-17 00:21:57 -0700

    added test to test success when using a channel

--- test/nodequeue_tests.js
+++ test/nodequeue_tests.js
@@ -31,7 +31,7 @@ exports.canConnect = vows.describe('canConnect').addBatch({
 			'client did connect': function(err){
 				assert.equal(err, null); 
 			},
-			'use channel': {
+			'test use channel error': {
 				topic: function(){
 					var cb = this.callback;
 					socket.on('data', function(data){
@@ -41,14 +41,39 @@ exports.canConnect = vows.describe('canConnect').addBatch({
 					socket.write("foobar");
 					
 				},
-				'use channel response success': function(err, data){
+				'use channel response error': function(err, data){
 					var json = JSON.parse(data.substr(0, data.length - 2));
-
 					assert.include(json, 'code');
 					assert.include(json, 'msg');
 					assert.include(json, 'data');
 					assert.equal(json.code, 100);
+				},
+				'test use channel success': {
+					topic: function(){
+						var cb = this.callback;
 
+						// gotta remove the listener to reassign it
+						socket.removeAllListeners('data');
+						socket.on('data', function(data){
+							cb(null, data.toString());
+						});
+
+						var request = JSON.stringify({
+							code: 0,
+							args: {
+								channel_name: "test_channel"
+							}
+						});
+						
+						socket.write(request + "\r\n");
+					},
+					'use channel response success': function(err, data){
+						var json = JSON.parse(data.substr(0, data.length - 2));
+						assert.include(json, 'code');
+						assert.include(json, 'msg');
+						assert.include(json, 'data');
+						assert.equal(json.code, 200);
+					}
 				}
 			}
 		}
